33ec4a0a030f75c77975ab1af688b9a77340ca63
Michael W Powell <mwpowellhtx@gmail.com>
2018-10-26 13:18:30 -0400

    added refactored assembly info bits from legacy desktop framework based projects

---
+++ src/Kingdom.OrTools.ConstraintSolver.Samples.Tests.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("044c9d08-2341-467a-85e7-cdb2bf480373")]
