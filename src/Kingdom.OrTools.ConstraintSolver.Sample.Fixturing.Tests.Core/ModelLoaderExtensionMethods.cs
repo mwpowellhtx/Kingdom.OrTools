b5b29e30bc24a58523d3d7e89c673a686026ee06
Michael W Powell <mwpowellhtx@gmail.com>
2019-04-05 17:33:36 -0400

    ditto commenting out mystery direction

--- src/Kingdom.OrTools.ConstraintSolver.Sample.Fixturing.Tests.Core/ModelLoaderExtensionMethods.cs
+++ src/Kingdom.OrTools.ConstraintSolver.Sample.Fixturing.Tests.Core/ModelLoaderExtensionMethods.cs
@@ -1,36 +1,36 @@
-﻿// ReSharper disable once IdentifierTypo
-namespace Kingdom.Constraints.Sample.Fixturing.Tests
-{
-    using Google.OrTools.ConstraintSolver;
+﻿//// ReSharper disable once IdentifierTypo
+//namespace Kingdom.Constraints.Sample.Fixturing.Tests
+//{
+//    using Google.OrTools.ConstraintSolver;
 
-    internal static class ModelLoaderExtensionMethods
-    {
-        // TODO: TBD: pretty sure this one was an API before...
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="loader"></param>
-        /// <param name="expressionName"></param>
-        /// <returns></returns>
-        /// <see cref="!:http://github.com/google/or-tools/issues/905">What happened to .NET ConstraintSolver CpModelLoader.IntegerExpressionByName</see>
-        internal static IntExpr IntegerExpressionByName(this CpModelLoader loader, string expressionName)
-        {
-            var maxIntegerExpressions = loader.NumIntegerExpressions();
-            for (var i = 0; i < maxIntegerExpressions; i++)
-            {
-                var expression = loader.IntegerExpression(i);
-                if (expression.Name() == expressionName)
-                {
-                    return expression;
-                }
+//    internal static class ModelLoaderExtensionMethods
+//    {
+//        // TODO: TBD: pretty sure this one was an API before...
+//        /// <summary>
+//        /// 
+//        /// </summary>
+//        /// <param name="loader"></param>
+//        /// <param name="expressionName"></param>
+//        /// <returns></returns>
+//        /// <see cref="!:http://github.com/google/or-tools/issues/905">What happened to .NET ConstraintSolver CpModelLoader.IntegerExpressionByName</see>
+//        internal static IntExpr IntegerExpressionByName(this CpModelLoader loader, string expressionName)
+//        {
+//            var maxIntegerExpressions = loader.NumIntegerExpressions();
+//            for (var i = 0; i < maxIntegerExpressions; i++)
+//            {
+//                var expression = loader.IntegerExpression(i);
+//                if (expression.Name() == expressionName)
+//                {
+//                    return expression;
+//                }
 
-                if (expression.Var().Name() == expressionName)
-                {
-                    return expression;
-                }
-            }
+//                if (expression.Var().Name() == expressionName)
+//                {
+//                    return expression;
+//                }
+//            }
 
-            return null;
-        }
-    }
-}
+//            return null;
+//        }
+//    }
+//}
--- src/Kingdom.OrTools.ConstraintSolver.Sample.Fixturing.Tests.Core/TestFixtureBase.cs
+++ src/Kingdom.OrTools.ConstraintSolver.Sample.Fixturing.Tests.Core/TestFixtureBase.cs
@@ -1,42 +1,42 @@
-﻿using System;
-using System.Linq;
+﻿//using System;
+//using System.Linq;
 
-// ReSharper disable once IdentifierTypo
-namespace Kingdom.Constraints.Sample.Fixturing.Tests
-{
-    using Xunit.Abstractions;
+//// ReSharper disable once IdentifierTypo
+//namespace Kingdom.Constraints.Sample.Fixturing.Tests
+//{
+//    using Xunit.Abstractions;
 
-    public abstract class TestFixtureBase : IDisposable
-    {
-        private ITestOutputHelper OutputHelper { get; }
+//    public abstract class TestFixtureBase : IDisposable
+//    {
+//        private ITestOutputHelper OutputHelper { get; }
 
-        protected void WriteLine(string message, params object[] args)
-        {
-            if (!args.Any())
-            {
-                OutputHelper.WriteLine(message);
-            }
-            else
-            {
-                OutputHelper.WriteLine(message, args);
-            }
-        }
+//        protected void WriteLine(string message, params object[] args)
+//        {
+//            if (!args.Any())
+//            {
+//                OutputHelper.WriteLine(message);
+//            }
+//            else
+//            {
+//                OutputHelper.WriteLine(message, args);
+//            }
+//        }
 
-        protected TestFixtureBase(ITestOutputHelper outputHelper)
-        {
-            OutputHelper = outputHelper;
-        }
+//        protected TestFixtureBase(ITestOutputHelper outputHelper)
+//        {
+//            OutputHelper = outputHelper;
+//        }
 
-        protected bool IsDisposed { get; private set; }
+//        protected bool IsDisposed { get; private set; }
 
-        protected virtual void Dispose(bool disposing)
-        {
-        }
+//        protected virtual void Dispose(bool disposing)
+//        {
+//        }
 
-        public void Dispose()
-        {
-            Dispose(true);
-            IsDisposed = true;
-        }
-    }
-}
+//        public void Dispose()
+//        {
+//            Dispose(true);
+//            IsDisposed = true;
+//        }
+//    }
+//}
