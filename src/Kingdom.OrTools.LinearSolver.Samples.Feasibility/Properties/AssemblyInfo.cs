37aa859b3fa69d67578be64869d11cdf506dfb4a
Michael W Powell <mwpowellhtx@gmail.com>
2019-04-05 17:36:49 -0400

    factored bits of assembly info into the simplify project level if they need to be there at all updated problem solver code around latest changes as well as around language features

--- src/Kingdom.OrTools.LinearSolver.Samples.Feasibility/Properties/AssemblyInfo.cs
+++ src/Kingdom.OrTools.LinearSolver.Samples.Feasibility/Properties/AssemblyInfo.cs
@@ -1,13 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Kingdom.Constraints.Samples.Feasibility")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -16,16 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("797397ef-1d04-4977-9c00-ff467a419965")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.1.*")]
-[assembly: AssemblyFileVersion("3.0.1.0")]
