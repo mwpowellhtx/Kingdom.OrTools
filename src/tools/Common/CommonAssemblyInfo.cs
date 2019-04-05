90f7e98bdd6b936568341857cb15a590a212e0ef
Michael W Powell <mwpowellhtx@gmail.com>
2019-04-05 17:17:44 -0400

    updated shared assembly info with details friendly copyright text

--- src/tools/Common/CommonAssemblyInfo.cs
+++ src/tools/Common/CommonAssemblyInfo.cs
@@ -5,6 +5,6 @@
 // associated with an assembly.
 [assembly: AssemblyProduct("Kingdom.OrTools")]
 [assembly: AssemblyCompany("Kingdom Software")]
-[assembly: AssemblyCopyright("Copyright © 2015-2018")]
+[assembly: AssemblyCopyright("Copyright (c) 2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
