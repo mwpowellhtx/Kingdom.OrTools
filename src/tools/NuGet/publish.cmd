f86db8fea1d1f3803e5eba064a93322798a7d799
Michael W Powell <mwpowellhtx@gmail.com>
2019-04-11 20:02:58 -0400

    updated the publish batch file should render the copy batch file obsolete but will hang onto that for now

--- src/tools/NuGet/publish.cmd
+++ src/tools/NuGet/publish.cmd
@@ -27,16 +27,21 @@ rem Setup All Projects
 set all_projects=Kingdom.OrTools.Core
 set all_projects=%all_projects%%delim%Kingdom.OrTools.ConstraintSolver.Core
 set all_projects=%all_projects%%delim%Kingdom.OrTools.LinearSolver.Core
+set all_projects=%all_projects%%delim%Kingdom.OrTools.Sat.Core
 rem Setup Constraint Solver
 set constraint_projects=Kingdom.OrTools.Core
 set constraint_projects=%constraint_projects%%delim%Kingdom.OrTools.ConstraintSolver.Core
 rem Setup Linear Solver
 set linear_projects=Kingdom.OrTools.Core
 set linear_projects=%linear_projects%%delim%Kingdom.OrTools.LinearSolver.Core
+rem Setup Sat Solver
+set sat_projects=Kingdom.OrTools.Core
+set sat_projects=%linear_projects%%delim%Kingdom.OrTools.Sat.Core
 rem Setup Solver Projects
 set solver_projects=Kingdom.OrTools.Core
 set solver_projects=%solver_projects%%delim%Kingdom.OrTools.ConstraintSolver.Core
 set solver_projects=%solver_projects%%delim%Kingdom.OrTools.LinearSolver.Core
+set solver_projects=%solver_projects%%delim%Kingdom.OrTools.Sat.Core
 
 
 :parse_args
@@ -60,6 +65,18 @@ if "%1" == "--dry-run" (
 if "%1" == "--config" (
     set config=%2
     shift
+    goto :next_arg
+)
+
+:set_publish_local
+if "%1" == "--local" (
+    set function=local
+    goto :next_arg
+)
+
+:set_publish_nuget
+if "%1" == "--nuget" (
+    set function=nuget
     goto :next_arg
 )
 
@@ -114,6 +131,13 @@ if "%1" == "--solvers" (
 	goto :next_arg
 )
 
+:add_sat_projects
+if "%1" == "--sat" (
+    rem Prepare to publish All Projects.
+    set projects=%sat_projects%
+    goto :next_arg
+)
+
 :add_all_projects
 if "%1" == "--all" (
     rem Prepare to publish All Projects.
@@ -143,6 +167,11 @@ goto :parse_args
 
 :verify_args
 
+:verify_function
+if "%function%" == "" (
+    set function=local
+)
+
 :verify_projects
 if "%projects%" == "" (
     rem In which case, there is nothing else to do.
@@ -159,6 +188,9 @@ if "%config%" == "" (
 :publish_projects
 
 :set_vars
+set xcopy_exe=xcopy.exe
+set xcopy_opts=/y /f
+set xcopy_dest_dir=G:\Dev\NuGet\local\packages
 rem Expecting NuGet to be found in the System Path.
 set nuget_exe=NuGet.exe
 set nuget_push_verbosity=detailed
@@ -178,7 +210,8 @@ if not "%projects%" == "" (
 :next_project
 if not "%projects%" == "" (
     for /f "tokens=1* delims=%delim%" %%p in ("%projects%") do (
-        call :publish_pkg %%p
+        if "%function%" == "nuget" call :publish_nuget %%p
+        if "%function%" == "local" call :publish_local %%p
         set projects=%%q
         goto :next_project
     )
@@ -188,7 +221,18 @@ popd
 
 goto :end
 
-:publish_pkg
+:publish_local
+for %%f in ("%1\bin\%config%\%1.*.nupkg") do (
+    if "%dry%" == "true" (
+        echo Dry run: %xcopy_exe% %%f %xcopy_dest_dir% %xcopy_opts%
+    ) else (
+        echo Running: %xcopy_exe% %%f %xcopy_dest_dir% %xcopy_opts%
+        %xcopy_exe% %%f %xcopy_dest_dir% %xcopy_opts%
+    )
+)
+exit /b
+
+:publish_nuget
 for %%f in ("%1\bin\%config%\%1.*.nupkg") do (
     if "%dry%" == "true" (
         echo Dry run: %nuget_exe% push "%%f"%nuget_push_opts%
