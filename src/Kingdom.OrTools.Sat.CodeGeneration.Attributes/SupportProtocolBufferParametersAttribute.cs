b16725826a6262a036afe0dc32ae6e0175300dad
Michael W Powell <mwpowellhtx@gmail.com>
2019-07-16 22:33:32 -0400

    added assembly code generation attribute

---
+++ src/Kingdom.OrTools.Sat.CodeGeneration.Attributes/SupportProtocolBufferParametersAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Kingdom.OrTools.Sat
+{
+    using Code.Generation.Roslyn;
+
+    /// <summary>
+    /// The Code Generator must be named CpSatParametersGenerator in the
+    /// Kingdom.OrTools.Sat namespace furnished from a separate assembly.
+    /// </summary>
+    /// <inheritdoc />
+    [AttributeUsage(AttributeTargets.Assembly)
+     , CodeGenerationAttribute("Kingdom.OrTools.Sat.CpSatParametersGenerator")
+     , Conditional("CodeGeneration")]
+    public class SupportProtocolBufferParametersAttribute : Attribute
+    {
+    }
+}
