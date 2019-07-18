323dcf25b6fde07050a783a4148ff52da96073a6
Michael W Powell <mwpowellhtx@gmail.com>
2019-07-18 15:33:01 -0400

    refactored enumerate method added get range method

--- src/Kingdom.OrTools.Sat.Parameters.Tests/TestFixtureBase.cs
+++ src/Kingdom.OrTools.Sat.Parameters.Tests/TestFixtureBase.cs
@@ -1,4 +1,7 @@
-﻿namespace Kingdom.OrTools.Sat.Parameters
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Kingdom.OrTools.Sat.Parameters
 {
     using Xunit.Abstractions;
 
@@ -9,6 +12,37 @@
         protected TestFixtureBase(ITestOutputHelper outputHelper)
         {
             OutputHelper = outputHelper;
+        }
+
+        /// <summary>
+        /// Returns the Range of <paramref name="values"/> as a fresh <see cref="IEnumerable{T}"/>
+        /// instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        protected static IEnumerable<T> GetRange<T>(params T[] values)
+        {
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            foreach (var x in values)
+            {
+                yield return x;
+            }
+        }
+
+        /// <summary>
+        /// Enumerates the weakly typed <paramref name="values"/> to a Sequence of
+        /// <see cref="object"/>.
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        protected static IEnumerable<object> Enumerate(IEnumerable values)
+        {
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            foreach (var x in values)
+            {
+                yield return x;
+            }
         }
     }
 }
