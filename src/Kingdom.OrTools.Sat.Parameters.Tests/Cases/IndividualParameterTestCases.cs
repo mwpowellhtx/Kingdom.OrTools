63413bc15a1a04a7f96359ef6814616cc8432357
Michael W Powell <mwpowellhtx@gmail.com>
2019-07-18 15:33:21 -0400

    rounded out several test cases

--- src/Kingdom.OrTools.Sat.Parameters.Tests/Cases/IndividualParameterTestCases.cs
+++ src/Kingdom.OrTools.Sat.Parameters.Tests/Cases/IndividualParameterTestCases.cs
@@ -6,6 +6,9 @@ namespace Kingdom.OrTools.Sat.Parameters
 {
     using static Double;
     using static Names;
+    using static Ordinals;
+    using static String;
+    using static ParameterValueRenderingOptions;
     using static AnnotatedWeekday;
 
     internal class IndividualParameterTestCases : ParameterTestCasesBase
@@ -32,18 +35,41 @@ namespace Kingdom.OrTools.Sat.Parameters
 
         private delegate IParameter<T> CreateParameterCallback<T>(T value);
 
-        // ReSharper disable once UnusedTypeParameter
+        private delegate IRepeatedParameter<T> CreateRepeatedParameterCallback<T>(T value, T[] others);
+
+        // ReSharper disable UnusedTypeParameter
         private delegate string RenderParameterValueCallback<TParameter, in T>(T value)
             where TParameter : IParameter<T>;
+
+        private delegate string RenderRepeatedParameterValueCallback<TParameter, in T>(IEnumerable<T> values)
+            where TParameter : IRepeatedParameter<T>;
+        // ReSharper restore UnusedTypeParameter
 
         private static RenderParameterValueCallback<TParameter, T> GetDefaultRenderParameterValueCallback<TParameter, T>(Func<T, string> render = null, string parameterName = null)
             where TParameter : IParameter<T>
             => x => $"{parameterName ?? typeof(TParameter).Name}={(render ?? (y => $"{y}")).Invoke(x)}";
 
+        private static RenderRepeatedParameterValueCallback<TParameter, T> GetDefaultRenderRepeatedParameterValueCallback<TParameter, T>(Func<IEnumerable<T>, string> render = null, string parameterName = null)
+            where TParameter : IRepeatedParameter<T>
+            => x =>
+            {
+                string RenderBits(IEnumerable<T> bits) => Join(",", FilterValues(bits).Select(y => $"{y}"));
+                return $"{parameterName ?? typeof(TParameter).Name}={(render ?? RenderBits).Invoke(x)}";
+            };
+
         private static IEnumerable<object> GetParameterTestCase<TParameter, T>(T value, CreateParameterCallback<T> create
             , RenderParameterValueCallback<TParameter, T> render, int? precision = null)
             where TParameter : IParameter<T>
-            => GetRange<object>(create(value), typeof(T), value, render(value), precision).ToArray();
+            => GetRange<object>(create(value)
+                , typeof(T), value, render(value), CurrentOrdinal, precision).ToArray();
+
+        // ReSharper disable PossibleMultipleEnumeration
+        private static IEnumerable<object> GetRepeatedParameterTestCase<TParameter, T>(IEnumerable<T> values, CreateRepeatedParameterCallback<T> create
+            , RenderRepeatedParameterValueCallback<TParameter, T> render, int? precision = null)
+            where TParameter : IRepeatedParameter<T>, new()
+            => GetRange<object>(values.Any() ? create(values.First(), values.Skip(1).ToArray()) : new TParameter()
+                , typeof(T), values, render(values), CurrentOrdinal, precision).ToArray();
+        // ReSharper restore PossibleMultipleEnumeration
 
         private static IEnumerable<object[]> _privateCases;
 
@@ -54,6 +80,8 @@ namespace Kingdom.OrTools.Sat.Parameters
                 string RenderDoubleValue(double x) => ParameterValueRenderingOptions.RenderDoubleValue(x);
 
                 string RenderBoolean(bool x) => $"{x}".ToLower();
+
+                string RenderRepeatedBoolean(IEnumerable<bool> bits) => Join(",", FilterValues(bits).Select(RenderBoolean));
 
                 // We do it this way because we are not here to use the code under test to inform the test case.
                 string RenderAnnotatedWeekday(AnnotatedWeekday x)
@@ -73,7 +101,11 @@ namespace Kingdom.OrTools.Sat.Parameters
 
                 string RenderAnnotatedEnumValue<T>(T x) => x is AnnotatedWeekday y ? RenderAnnotatedWeekday(y) : $"{x}";
 
-                IEnumerable<object[]> GetAll()
+                // ReSharper disable PossibleMultipleEnumeration
+                string RenderAnnotatedEnumValues<T>(IEnumerable<T> x) => Join(",", FilterValues(x).Select(RenderAnnotatedEnumValue));
+                // ReSharper restore PossibleMultipleEnumeration
+
+                IEnumerable<object[]> GetAllSingularParameters()
                 {
                     // Default Boolean is False, correct...
                     yield return GetParameterTestCase(default, _ => new BooleanParameter(), GetDefaultRenderParameterValueCallback<BooleanParameter, bool>(RenderBoolean)).ToArray();
@@ -149,7 +181,52 @@ namespace Kingdom.OrTools.Sat.Parameters
                     }
                 }
 
-                return _privateCases ?? (_privateCases = GetAll().ToArray());
+                IEnumerable<object[]> GetAllRepeatedParameters()
+                {
+                    var boolValues = GetRange(default, true).ToArray();
+
+                    for (var i = 0; i <= boolValues.Length; ++i)
+                    {
+                        yield return GetRepeatedParameterTestCase(boolValues.Take(i).ToArray(), (x, y) => new BooleanRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<BooleanRepeatedParameter, bool>(RenderRepeatedBoolean)).ToArray();
+                        yield return GetRepeatedParameterTestCase(boolValues.Take(i).ToArray(), (x, y) => new AnnotatedBooleanRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<AnnotatedBooleanRepeatedParameter, bool>(RenderRepeatedBoolean, annotated_boolean_repeated)).ToArray();
+                    }
+
+                    var intValues = GetRange(1, 2, 3).ToArray();
+
+                    for (var i = 0; i <= intValues.Length; ++i)
+                    {
+                        yield return GetRepeatedParameterTestCase(intValues.Take(i).ToArray(), (x, y) => new IntegerRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<IntegerRepeatedParameter, int>()).ToArray();
+                        yield return GetRepeatedParameterTestCase(intValues.Take(i).ToArray(), (x, y) => new AnnotatedIntegerRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<AnnotatedIntegerRepeatedParameter, int>(parameterName: annotated_integer_repeated)).ToArray();
+                    }
+
+                    var longValues = GetRange<long>(1, 2, 3).ToArray();
+
+                    for (var i = 0; i <= longValues.Length; ++i)
+                    {
+                        yield return GetRepeatedParameterTestCase(longValues.Take(i).ToArray(), (x, y) => new LongRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<LongRepeatedParameter, long>()).ToArray();
+                        yield return GetRepeatedParameterTestCase(longValues.Take(i).ToArray(), (x, y) => new AnnotatedLongRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<AnnotatedLongRepeatedParameter, long>(parameterName: annotated_long_repeated)).ToArray();
+                    }
+
+                    var monthValues = GetEnumValues<Month>().ToArray();
+
+                    for (var i = 0; i <= monthValues.Length; ++i)
+                    {
+                        yield return GetRepeatedParameterTestCase(monthValues.Take(i).ToArray(), (x, y) => new MonthRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<MonthRepeatedParameter, Month>(RenderAnnotatedEnumValues)).ToArray();
+                        yield return GetRepeatedParameterTestCase(monthValues.Take(i).ToArray(), (x, y) => new AnnotatedMonthRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<AnnotatedMonthRepeatedParameter, Month>(RenderAnnotatedEnumValues, annotated_month_repeated)).ToArray();
+                    }
+
+                    var weekdayValues = GetEnumValues<AnnotatedWeekday>().ToArray();
+
+                    for (var i = 0; i <= weekdayValues.Length; ++i)
+                    {
+                        yield return GetRepeatedParameterTestCase(weekdayValues.Take(i).ToArray(), (x, y) => new WeekdayRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<WeekdayRepeatedParameter, AnnotatedWeekday>(RenderAnnotatedEnumValues)).ToArray();
+                        yield return GetRepeatedParameterTestCase(weekdayValues.Take(i).ToArray(), (x, y) => new AnnotatedWeekdayRepeatedParameter(x, y), GetDefaultRenderRepeatedParameterValueCallback<AnnotatedWeekdayRepeatedParameter, AnnotatedWeekday>(RenderAnnotatedEnumValues, annotated_weekday_repeated)).ToArray();
+                    }
+
+                    // TODO: TBD: next is to itemize the litany of Double cases...
+                }
+
+                return _privateCases ?? (_privateCases = GetAllSingularParameters().Concat(GetAllRepeatedParameters()).ToArray());
             }
         }
 
