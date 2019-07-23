e2131abd97bc1f552cbc7b332f76a8dc72d8bb9d
Michael W Powell <mwpowellhtx@gmail.com>
2019-07-23 16:34:03 -0400

    and which further lends itself to a fairly robust slicing dissection of the same for collection verification

---
+++ src/Kingdom.OrTools.Sat.Parameters.Tests/Cases/ParameterCollectionRenderingTestCases.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kingdom.OrTools.Sat.Parameters
+{
+    using static Characters;
+    using static String;
+
+    internal class ParameterCollectionRenderingTestCases : ParameterTestCasesBase
+    {
+        private static IEnumerable<object[]> _privateCases;
+
+        protected override IEnumerable<object[]> Cases
+        {
+            get
+            {
+                // ReSharper disable PossibleMultipleEnumeration
+                IEnumerable<object[]> GetAll()
+                {
+                    IEnumerable<TestCaseDescriptor> descriptors = Descriptors.ToArray();
+
+                    while (descriptors.TrySliceOverCollection(7, out var slice, out descriptors))
+                    {
+                        var parameters = slice.Select(p => p.Instance).ToArray();
+
+                        yield return GetRange<object>(
+                            new ParameterCollection(slice.Select(p => p.Instance))
+                            , parameters
+                            , Join($"{SemiColon}", slice.Select(p => p.Rendered))
+                        ).ToArray();
+                    }
+                }
+                // ReSharper restore PossibleMultipleEnumeration
+
+                return _privateCases ?? (_privateCases = GetAll().ToArray());
+            }
+        }
+    }
+}
