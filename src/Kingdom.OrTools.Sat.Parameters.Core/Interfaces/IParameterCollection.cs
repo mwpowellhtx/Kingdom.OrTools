3722a3157b93768cf1998819ee8a6fc4a9bae4e6
Michael W Powell <mwpowellhtx@gmail.com>
2019-07-23 16:29:07 -0400

    added parameter collection assets

---
+++ src/Kingdom.OrTools.Sat.Parameters.Core/Interfaces/IParameterCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Kingdom.OrTools.Sat.Parameters
+{
+    using IParameterCollectionType = ICollection<IParameter>;
+
+    /// <inheritdoc />
+    public interface IParameterCollection : IParameterCollectionType
+    {
+        /// <summary>
+        /// Renders the Collection given a nominal set of <paramref name="options"/>.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        string ToString(IParameterValueRenderingOptions options);
+    }
+}
