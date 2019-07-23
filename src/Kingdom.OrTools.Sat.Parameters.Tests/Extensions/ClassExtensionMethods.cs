a4c121fbfe782f30d041134b6cb7685d21a60220
Michael W Powell <mwpowellhtx@gmail.com>
2019-07-23 16:31:22 -0400

    added class extension methods

---
+++ src/Kingdom.OrTools.Sat.Parameters.Tests/Extensions/ClassExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kingdom.OrTools.Sat.Parameters
+{
+    internal static class ClassExtensionMethods
+    {
+        /// <summary>
+        /// Initializes the <paramref name="instance"/> given an optional
+        /// <paramref name="callback"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="instance"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public static T Initialize<T>(this T instance, Action<T> callback = null)
+        {
+            callback?.Invoke(instance);
+            return instance;
+        }
+    }
+}
