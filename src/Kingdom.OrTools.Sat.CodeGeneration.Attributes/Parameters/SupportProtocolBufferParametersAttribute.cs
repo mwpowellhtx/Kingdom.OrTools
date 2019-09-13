f8d48ba3459951cdcd4ae4dd63e6513affadfffb
Michael W Powell <mwpowellhtx@gmail.com>
2019-09-13 14:24:47 -0400

    not sure what happened here some sort of corruption did some attributes refactoring

--- src/Kingdom.OrTools.Sat.CodeGeneration.Attributes/Parameters/SupportProtocolBufferParametersAttribute.cs
+++ src/Kingdom.OrTools.Sat.CodeGeneration.Attributes/Parameters/SupportProtocolBufferParametersAttribute.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace Kingdom.OrTools.Sat
+namespace Kingdom.OrTools.Sat.CodeGeneration.Attributes
 {
     using Code.Generation.Roslyn;
 
     /// <summary>
     /// The Code Generator must be named CpSatParametersGenerator in the
-    /// Kingdom.OrTools.Sat namespace furnished from a separate assembly.
+    /// Kingdom.OrTools.Sat.CodeGeneration.Generators namespace furnished from a separate
+    /// assembly.
     /// </summary>
     /// <inheritdoc />
     [AttributeUsage(AttributeTargets.Assembly)
-     , CodeGenerationAttribute("Kingdom.OrTools.Sat.CpSatParametersGenerator")
+     , CodeGenerationAttribute("Kingdom.OrTools.Sat.CodeGeneration.Generators.CpSatParametersAssemblyCodeGenerator, Kingdom.OrTools.Sat.CodeGeneration")
      , Conditional("CodeGeneration")]
     public class SupportProtocolBufferParametersAttribute : Attribute
     {
