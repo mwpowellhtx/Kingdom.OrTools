e5e0d16802bde50a28131c632f9251a509a6078e
Michael W Powell <mwpowellhtx@gmail.com>
2019-09-13 14:51:35 -0400

    not sure what happened here some sort of corruption initially we started on a pure code generation path however we are having some difficulty persuading the CLI environment to load the correct set of assemblies supporting the generator assembly also may or may not need the code generation or verify bits properties for now but will leave it be for the moment

---
+++ src/Kingdom.OrTools.Sat.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿//using Kingdom.OrTools.Sat.CodeGeneration.Attributes;
+
+///* Which is informed by the `*CodeGeneration´ Build Configurations, and requires that
+// * the `Kingdom.OrTools.Sat.CodeGeneration.Attributes´ package be build and deployed
+// * to internal NuGet Package Sources. */
+//[assembly: SupportProtocolBufferParameters]
