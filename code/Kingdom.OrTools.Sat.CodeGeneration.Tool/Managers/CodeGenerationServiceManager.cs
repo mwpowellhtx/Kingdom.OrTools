2dc145ea9b8e528c06f831c8406b04383efa7b5f
Michael W Powell <mwpowellhtx@gmail.com>
2019-09-28 13:23:47 -0400

    refactoring code generation bits

---
+++ code/Kingdom.OrTools.Sat.CodeGeneration.Tool/Managers/CodeGenerationServiceManager.cs
@@ -0,0 +1,21 @@
+﻿namespace Kingdom.OrTools.Sat.CodeGeneration
+{
+    using Code.Generation.Roslyn;
+
+    /// <inheritdoc />
+    internal class CodeGenerationServiceManager : ServiceManager<GeneratedSyntaxTreeDescriptor
+        , OrToolsSatGeneratedSyntaxTreeRegistry, OrToolsSatGeneratedSyntaxTreeRegistryJsonConverter>
+    {
+        /// <summary>
+        /// Internal Constructor.
+        /// </summary>
+        /// <inheritdoc />
+        /// <remarks>Leverages Implicit type conversion Operators implemented
+        /// by the Internal Registry and Data Transfer Object classes.</remarks>
+        internal CodeGenerationServiceManager(string outputDirectory, string registryFileName)
+            : base(outputDirectory, registryFileName
+                , () => OrToolsSatGeneratedSyntaxTreeRegistryJsonConverter.Converter)
+        {
+        }
+    }
+}
---
+++ code/Kingdom.OrTools.Sat.CodeGeneration.Tool/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Kingdom.OrTools.Sat.CodeGeneration
+{
+    using static NConsole.Options.ConsoleManager;
+
+    /// <summary>
+    /// The Program class.
+    /// </summary>
+    /// <remarks>Evaluate whether there should be any tooling updates, never mind deeper into the
+    /// Protocol Buffer ANTLR parser concerns, based on whether the `sat_parameters.proto´ has
+    /// updated at all since the most recent baselines.</remarks>
+    /// <see cref="!:https://github.com/google/or-tools/blob/stable/ortools/sat/sat_parameters.proto"/>
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            using (var instance = CodeGenerationConsoleManager.Instance)
+            {
+                // ReSharper disable once InvertIf
+                if (instance.TryParseOrShowHelp(args))
+                {
+                    instance.Run(out var errorLevel);
+                    return errorLevel;
+                }
+            }
+
+            // TODO: TBD: return default here? or another code?
+            return DefaultErrorLevel;
+        }
+    }
+}
