4ae3e154e52cc984f080b1faea734ddf3367cf53
Michael W Powell <mwpowellhtx@gmail.com>
2019-09-27 21:26:54 -0400

    refactored core related bits into the core project assembly itself

--- src/Kingdom.OrTools.Sat.Parameters/Implementation/RenderParameterValueCallback.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kingdom.OrTools.Sat.Parameters
-{
-    /// <summary>
-    /// Callback used to Render a <paramref name="value"/> <see cref="string"/>.
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    public delegate string RenderParameterValueCallback(object value);
-}
