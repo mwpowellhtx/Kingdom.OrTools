d836bb93864e2a2b38ac4d8bf6cc7715d089690d
Michael W. Powell <mwpowelllde@gmail.com>
2019-10-13 10:37:43 -0400

    added key clarifying comments concerning the utility of the Anchor class

--- src/Kingdom.OrTools.Sat.Parameters/Anchor.cs
+++ src/Kingdom.OrTools.Sat.Parameters/Anchor.cs
@@ -1,5 +1,9 @@
 ﻿namespace Kingdom.OrTools.Sat.Parameters
 {
+    // TODO: TBD: Anchor serves as a clue, as a Namespace reference, and not least of which as
+    // a clue that we should investigate the Parameters unit testing solution for further details
+    // concerning alignment verification with the latest Google.OrTools releases.
+
     /// <summary>
     /// Provides an Anchor class for internal use.
     /// We use this type to inform the unit tests as to the Parameters Assembly.
@@ -7,7 +11,10 @@
     /// <remarks>Predicate updates herewith based on whether there were changes from the most
     /// recent `sat_parameters.proto´ baseline. Such deltas may impact the code generation out
     /// workings, at best. Worst case, we may need to revisit the CG tooling itself, or even
-    /// deeper into the actual Protocol Buffer ANTLR bits.</remarks>
+    /// deeper into the actual Protocol Buffer ANTLR bits. Critical note, see also the Parameters
+    /// unit testing solution for further details concerning Google.OrTools Parameters Protocol
+    /// Buffer specification detailed analysis. This is of particular importance because we verify
+    /// some expected tallies further supported by the commit logs themselves.</remarks>
     /// <see cref="!:https://github.com/google/or-tools/blob/stable/ortools/sat/sat_parameters.proto"/>
     internal static class Anchor
     {
